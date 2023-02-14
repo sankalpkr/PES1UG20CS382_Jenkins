8e02417dd18737e175ac8bb244f27ef85a07eba5
Sankalp <sankalp042@gmail.com>
2023-02-14 21:57:20 +0530

    Pushing Jenkins Files

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "This is Sankalp Kumar, SRN : PES1UG20CS382\n";
+  return 0;
+}
