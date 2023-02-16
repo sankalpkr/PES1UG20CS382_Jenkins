67b2cd4db8a374c181c86a91bf51bcc7b86c4017
sankalpkr <79364524+sankalpkr@users.noreply.github.com>
2023-02-16 22:00:45 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,5 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This is Sankalp Kumar, SRN : PES1UG20CS382\n";
-  cout << "Hello again, This line is added later to test webhook trigger\n";
   return 0;
 }
